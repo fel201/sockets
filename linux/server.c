7313901c331e1dd4ae7ab5345dfc4becc3928d38
fmps <fmps@g3c21.cin.ufpe.br>
2025-11-14 14:06:31 -0300

    .

---
+++ linux/server.c
@@ -0,0 +1,41 @@
+#define _XOPEN_SOURCE 700
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#define LOCALHOST "127.0.0.1"
+int main(void) {
+    struct addrinfo hints, *res;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM; // uses TCP
+
+    int status;
+    if ((status = getaddrinfo(NULL, "3490", &hints, &res)) != 0) {
+        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    int socket_file_descriptor = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    bind(socket_file_descriptor, res->ai_addr, res->ai_addrlen);
+    listen(socket_file_descriptor, 20);
+
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    int new_fd;
+    while(1) {
+        addr_size = sizeof their_addr;
+        new_fd = accept(socket_file_descriptor, (struct sockaddr *)&their_addr, &addr_size);
+    }
+    return 0;
+}
